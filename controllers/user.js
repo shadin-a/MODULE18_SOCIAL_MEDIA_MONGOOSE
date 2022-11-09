a68246cb1373cb5732c4ec6c0c4550e2694876bd
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-08 20:02:28 -0800

    user controller mocked

---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+const app = require("express").Router();
+const Thought = require("../models/thought");
+const User = require("../models/user");
+
+module.exports = {
+    //GET ALL USERS
+
+    //GET USER BY ID
+
+    //CREATE A NEW USER
+
+    //UPDATE USER BY ID
+
+    //DELETE A USER BY ID
+
+    //ADD A FRIEND TO USER FREINDS
+
+    //DELETE A FRIEND FROM USER FRIENDS
+    
+}

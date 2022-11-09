cb0383bd8e1114f4a78e926046cb4a5936375250
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-08 21:09:53 -0800

    user controller file completed

--- controllers/user.js
+++ controllers/user.js
@@ -19,12 +19,14 @@ module.exports = {
                 : res.json(user)))
             .catch((err) => res.status(500).json(err));
     },
+    
     //CREATE A NEW USER
     createUser(req, res) {
         User.create(req.body)
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
+
     //UPDATE USER BY ID
     updateUser(req, res) {
         User.findByIdAndUpdate(
@@ -33,15 +35,37 @@ module.exports = {
                 username: req.body.username,
                 email: req.body.email,
             },
-        )
-            .then((user) => res.json(user))
+        ).then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
-}
+
     //DELETE A USER BY ID
+    deleteUser(req, res) {
+        User.findByIdAndDelete(
+            req.params.id
+        ).then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
 
     //ADD A FRIEND TO USER FREINDS
+    addFriend(req, res) {
+        User.findByIdAndUpdate(
+            req.params.userId , 
+            { $addToSet: 
+            { friends: req.params.friendId } 
+        })
+          .then((user) => res.json(user))
+          .catch((err) => res.status(500).json(err));
+      },
 
     //DELETE A FRIEND FROM USER FRIENDS
-
+      deleteFriend(req,res) {
+        User.findByIdAndDelete(
+            req.params.userId,
+            {$pull:
+            {friends: req.params.friendId }}
+        )
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+      }
 }

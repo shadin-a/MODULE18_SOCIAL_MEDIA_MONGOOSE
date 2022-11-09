edcf754ea33e7ccd347b05726f2e072e80b0db74
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-08 20:00:00 -0800

    mocked the thought routes in controller

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -1,1 +1,21 @@
 //THE ACTUAL ROUTES FOR EACH ELEMENT OF THE USER STORY FOR THOUGHTS
+const app = require("express").Router();
+const Thought = require("../models/thought");
+const User = require("../models/user");
+
+module.exports = {
+    //GET A THOT (ALL)
+
+    //GET A THOT BY ID
+
+    //POST A NEW THOT
+
+    //UPDATE EXISTING THOT
+
+    //DELETE EXISTING THOT
+
+    //POST A THOT REACTION
+
+    //DELETE A THOT REACTION
+
+}

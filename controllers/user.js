dd9643900018c8b2841695ecb8761c0175708179
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-08 21:01:05 -0800

    update user by id added

--- controllers/user.js
+++ controllers/user.js
@@ -14,19 +14,30 @@ module.exports = {
     //GET USER BY ID
     getSingleUser(req, res) {
         User.findById(req.params.id)
-        .then((user) => (!user
-            ? res.status(404).json({ message: 'Cannot find that user!' })
-            : res.json(user)))
-          .catch((err) => res.status(500).json(err));
+            .then((user) => (!user
+                ? res.status(404).json({ message: 'Cannot find that user!' })
+                : res.json(user)))
+            .catch((err) => res.status(500).json(err));
     },
     //CREATE A NEW USER
     createUser(req, res) {
         User.create(req.body)
-        .then((user) => res.json(user))
-        .catch((err) => res.status(500).json(err));
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
     },
     //UPDATE USER BY ID
-
+    updateUser(req, res) {
+        User.findByIdAndUpdate(
+            req.params.id,
+            {
+                username: req.body.username,
+                email: req.body.email,
+            },
+        )
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+}
     //DELETE A USER BY ID
 
     //ADD A FRIEND TO USER FREINDS

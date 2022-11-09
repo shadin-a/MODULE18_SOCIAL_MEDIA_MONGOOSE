14fbb1543fe81a1925a288dd7e88de4360a394da
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-08 20:48:09 -0800

    user get routes added

--- controllers/user.js
+++ controllers/user.js
@@ -3,10 +3,22 @@ const Thought = require("../models/thought");
 const User = require("../models/user");
 
 module.exports = {
+
     //GET ALL USERS
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
 
     //GET USER BY ID
-
+    getSingleUser(req, res) {
+        User.findById(req.params.id)
+        .then((user) => (!user
+            ? res.status(404).json({ message: 'Cannot find that user!' })
+            : res.json(user)))
+          .catch((err) => res.status(500).json(err));
+    },
     //CREATE A NEW USER
 
     //UPDATE USER BY ID
@@ -16,5 +28,5 @@ module.exports = {
     //ADD A FRIEND TO USER FREINDS
 
     //DELETE A FRIEND FROM USER FRIENDS
-    
+
 }
